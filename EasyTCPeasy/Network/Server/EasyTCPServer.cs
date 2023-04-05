26c90621a7e4f4e8feb61613c77314415042b89e
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-04-05 20:52:28 +1000

    Initial server and client class.

---
+++ EasyTCPeasy/Network/Server/EasyTCPServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTCPeasy.Network.Server
+{
+    public class EasyTCPServer
+    {
+    }
+}
